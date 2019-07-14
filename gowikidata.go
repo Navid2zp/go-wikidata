088edb3900b8703f585a5986daf90f366366f725
Navid Zarepak <navid2zp@gmail.com>
2019-07-14 19:13:07 +0430

    fixed SetProperty for getclaims

--- gowikidata.go
+++ gowikidata.go
@@ -134,8 +134,8 @@ func (e *Entity) NewGetClaims() (*WikiDataGetClaimsRequest, error) {
 }
 
 // Param: property
-func (r *WikiDataGetClaimsRequest) SetProperty(property string) {
-	r.setParam("property", &[]string{property})
+func (r *WikiDataGetClaimsRequest) SetProperty(properties []string) {
+	r.setParam("property", &properties)
 }
 
 // Param: rank
